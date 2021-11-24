e86ea01c650672995cdc03f2243494d429326b0f
Andrei Shved <svensk.tut.by@gmail.com>
2021-11-24 21:22:55 +0300

    feat: add sidebar items dynamically

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DataHandlerService } from '../../service/data-handler.service';
+import { Category } from '../../model/Category';
+
 @Component({
     selector: 'app-categories',
     templateUrl: './categories.component.html',
     styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent implements OnInit {
-    constructor() {}
+    categories: Category[];
+    constructor(private dataHandler: DataHandlerService) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.categories = this.dataHandler.getCategories();
+    }
 }
