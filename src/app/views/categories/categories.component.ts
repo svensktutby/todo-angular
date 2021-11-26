3049dcb439605402fa252099b66474b60068df06
Andrei Shved <svensk.tut.by@gmail.com>
2021-11-26 23:06:56 +0300

    feat: add active class for category

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -10,6 +10,7 @@ import { Category } from '../../model/Category';
 })
 export class CategoriesComponent implements OnInit {
     categories: Category[];
+    selectedCategory: Category;
     constructor(private dataHandler: DataHandlerService) {}
 
     ngOnInit() {
@@ -17,6 +18,7 @@ export class CategoriesComponent implements OnInit {
     }
 
     showTasksByCategory(category: Category) {
+        this.selectedCategory = category;
         this.dataHandler.fillTasksByCategory(category);
     }
 }
